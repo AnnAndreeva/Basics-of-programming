d1319ef9f8d34eaec9cd179ef65522b6ee30dd51
Anna Andreeva <annandreeva21@gmail.com>
2022-08-28 20:01:48 +0400

    LR 07

---
+++ 1_semestr/ЛР07/ЛР07/Lines.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР07
+{
+    class Lines
+    {
+        public string s;
+        public Lines()
+        {
+        }        public Lines(string s)
+        {
+            this.s = s;
+        }
+
+        //Получение слова
+        static void GetWord(string s, int fp, int lp, int length, int c/*кол-во слов*/)
+        {
+            String[] word = s.Split(new char[] { ' ', ',', '-', ';', ':', '.', '!', '?' });
+
+        }
+
+        //Подсчет в строке числа букв
+        public int LetterCounter(string s)
+        {
+            int k = 0;
+            s.ToLower();
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    k++;
+                }
+            }
+            return k;
+        }
+
+        //Подсчет в строке средней длины слова
+        public double MiddleCounter(string s)
+        {
+            double k = 0;
+            string wrd;
+            s.ToLower();
+            String[] words = s.Split(new char[] { ' ', ',', '-', ';', ':', '.', '!', '?' });
+            for (int i = 0; i <= words.Length - 1; i++)
+            {
+                wrd = words[i];
+                k += words[i].Length;
+            }
+            k /= words.Length;
+            return k;
+        }
+
+
+        //Замена в строке все вхождения заданного пользователем слова
+        public string Change(string s, string word, string change)
+        {
+            int fp = 0;
+            string wrd, s1 = "";
+            s1 = s.ToLower();
+            String[] words = s1.Split(new char[] { ' ', ',', '-', ';', ':', '.', '!', '?' });
+            for (int i = 0; i <= words.Length - 1; i++)
+            {
+                wrd = words[i];
+                if (wrd == word)
+                {
+                    s1 = s1.Remove(fp, words[i].Length);
+                    wrd = wrd.Replace(words[i], change);
+                    s1 = s1.Insert(fp, wrd);
+                }
+                if (char.IsLetter(s1[words[i].Length + 2]))
+                {
+                    fp += words[i].Length + 1;
+                }
+                else
+                {
+                    int j = 1;
+                    while (!char.IsLetter(s1[words[i].Length + j]))
+                    {
+                        j++;
+                    }
+                    fp += words[i].Length + j - 1;
+                }
+            }
+            return s1;
+        }
+
+        //Подсчет в строке количество вхождений заданной подстроки
+        public int SubSCounter(string s, string subS)
+        {
+            int i = 0;
+            string s1;
+            s1 = s.ToLower();
+            i = (s1.Length - s1.Replace(subS, "").Length) / subS.Length;
+
+            return i;
+        }
+
+        //Проверка, является ли строка палиндромом
+        public void IsPalindrome(string s)
+        {
+            string s1 = s.ToLower();
+            String[] words = s1.Split(new char[] { ' ', ',', '-', ';', ':', '.', '!', '?' });
+            s1 = string.Join("", words);
+            string copyS1 = s1;
+            copyS1 = ReverseString(copyS1);
+            if (copyS1 == s1)
+            {
+                Console.WriteLine("Это палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Это не палиндром");
+            }
+
+        }
+
+        //Перевертывание строки
+        string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        //Получение числа
+        static void GetDigit(string s, int fp, int lp, int length)
+        {
+            int l = s.Length;
+            while ((fp <= l) && !(char.IsDigit(s[fp])))
+            {
+                fp++;
+            }
+            lp = fp;
+            while ((lp <= l) && (char.IsDigit(s[lp])))
+            {
+                lp++;
+            }
+            length = lp - fp + 1;
+            lp--;
+
+        }
+
+        //Проверка, является ли введенная строка датой
+        public void IsData(string s)
+        {
+            int data = 0;
+            int[] dig = new int[3];
+            string Sdig;
+            String[] words = s.Split(new char[] { '.' });
+            if (words.Length == 3)//проверяем кол-во слов в строке
+            {
+                for (int i = 0; i <= words.Length - 1; i++)
+                {
+                    Sdig = words[i];
+                    //Console.WriteLine(words[i]);
+                    int p = 0;
+                    for (int j = 0; j <= Sdig.Length-1; j++)
+                    {
+                        //проверяем, является ли слово числом
+                       if (char.IsDigit(Sdig[j]))
+                        {
+                            p++;
+                        }
+                       
+                    }
+                    if (p == Sdig.Length)
+                    {
+                        dig[i] = Convert.ToInt32(Sdig);
+                       
+                    }
+                    else
+                    {
+                        data = 0;
+                    }
+                }
+            }
+            else
+            {
+                data = 0;
+            }
+
+            int k = 0;
+            if (dig[k] > 0)
+            {
+                if (dig[k] <= 31)
+                {
+                    if (dig[k + 1] <= 12)
+                    {
+                        if ((dig[k] > 29) && (dig[k + 1] == 2))
+                        {
+                            data = 0;
+                        }
+                        else if ((dig[k] > 30) && ((dig[k + 1] == 4)|| (dig[k + 1] == 6)|| (dig[k + 1] == 9)|| (dig[k + 1] == 11))) {
+                            data = 0;
+                        }
+                        else {
+                            if ((dig[k + 2] / 100 == 0) || (dig[k + 2] / 10000 == 0))
+                            {
+                                data = 3;
+                            }
+                        }
+                        
+                    }
+                }
+
+            }
+            if (data == 3){
+                Console.WriteLine("Это дата");
+            }
+            else
+            {
+                Console.WriteLine("Это не дата");
+            }
+
+
+
+        }
+    }
+}
---
+++ 1_semestr/ЛР07/ЛР07/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool key = false;
+            while (key == false)
+            {
+                Console.WriteLine("Выберите задание Лабораторной работы №7");
+                Console.WriteLine("1 - подсчитать в строке число букв;");
+                Console.WriteLine("2 - подсчитать в строке среднюю длину слова");
+                Console.WriteLine("3 - заменить в строке все вхождения заданного пользователем слова");
+                Console.WriteLine("4 - подсчитать в строке количество вхождений заданной подстроки");
+                Console.WriteLine("5 - проверить, является ли строка палиндромом ");
+                Console.WriteLine("6 - проверить, является ли введенная строка датой\n");
+                Console.Write("Выбранное задание - ");
+                string menu1 = Console.ReadLine();
+                Console.WriteLine();
+                switch (menu1)
+                {
+                    case "1":
+                        {
+                            Task1();
+                            Console.WriteLine();
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "2":
+                        {
+                            Task2();
+                            Console.WriteLine();
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "3":
+                        {
+                            Task3();
+                            Console.WriteLine();
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "4":
+                        {
+                            Task4();
+                            Console.WriteLine();
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "5":
+                        {
+                            Task5();
+                            Console.WriteLine();
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "6":
+                        {
+                            Task6();
+                            Console.WriteLine();
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Введено значение, не соответствующее ни одному пункту меню.");
+                            Console.WriteLine("Для возвращения в главное меню нажмите Enter");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                }
+            }
+        }
+
+        public static void Task1()
+        {
+            string s;
+            int k;
+            Lines lines = new Lines();
+            Console.WriteLine("Подсчет в строке числа букв");
+            Console.WriteLine("Введите строку:");
+            s = Console.ReadLine();
+            k = lines.LetterCounter(s);
+            Console.WriteLine("Количество букв в строке = " + k);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static void Task2()
+        {
+            string s;
+            double k;
+            Lines lines = new Lines();
+            Console.WriteLine("Подсчет в строке средней длины слова");
+            Console.WriteLine("Введите строку:");
+            s = Console.ReadLine();
+            k = lines.MiddleCounter(s);
+            Console.WriteLine("Средняя длина слова в строке = {0:0.0}", k);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static void Task3()
+        {
+            string s, word, change, s1;
+            Lines lines = new Lines();
+            Console.WriteLine("Замена в строке все вхождения заданного пользователем слова");
+            s = "Привет – самое распространенное приветствие, привет также можно передать";
+            Console.WriteLine(s);
+            Console.Write("Введите слово, которое хотите заменить:");
+            word = Console.ReadLine();
+            Console.Write("Введите слово, на которое хотите заменить:");
+            change = Console.ReadLine();
+            s1 = lines.Change(s, word, change);
+            Console.WriteLine("Результат:");
+            Console.WriteLine(s1);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static void Task4()
+        {
+            string s, subS, s1;
+            int k;
+            Lines lines = new Lines();
+            Console.WriteLine("Подсчет в строке количество вхождений заданной подстроки");
+            s = "Привет – самое распространенное приветствие, привет также можно передать";
+            Console.WriteLine(s);
+            Console.Write("Введите подстроку:");
+            subS = Console.ReadLine();
+            k = lines.SubSCounter(s, subS);
+            Console.WriteLine("Количество вхождений заданной подстроки = " + k);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static void Task5()
+        {
+            string s;
+            Lines lines = new Lines();
+            Console.WriteLine("Проверка, является ли строка палиндромом");
+            Console.WriteLine("Введите строку:");
+            s = Console.ReadLine();
+            lines.IsPalindrome(s);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        public static void Task6()
+        {
+            string s;
+            Lines lines = new Lines();
+            Console.WriteLine("Проверка, является ли введенная строка датой");
+            Console.WriteLine("Введите строку:");
+            s = Console.ReadLine();
+            lines.IsData(s);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+    }
+}

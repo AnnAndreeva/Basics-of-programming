b895de7529b5910abbe368dd7d2a5b65040a0361
Anna Andreeva <annandreeva21@gmail.com>
2022-08-28 20:04:43 +0400

    Ping Pong game (extra task)

---
+++ 2_semestr/PingPong/PingPong/Game.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PingPong
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.time = new System.Windows.Forms.Label();
+            this.lScore = new System.Windows.Forms.Label();
+            this.ball2 = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.ball1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Controls.Add(this.time);
+            this.gamePanel.Controls.Add(this.lScore);
+            this.gamePanel.Controls.Add(this.ball2);
+            this.gamePanel.Controls.Add(this.racket);
+            this.gamePanel.Controls.Add(this.ball1);
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(800, 450);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time.Location = new System.Drawing.Point(357, 24);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(12, 20);
+            this.time.TabIndex = 5;
+            this.time.Text = "l";
+            this.time.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lScore
+            // 
+            this.lScore.AutoSize = true;
+            this.lScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lScore.Location = new System.Drawing.Point(357, 0);
+            this.lScore.Name = "lScore";
+            this.lScore.Size = new System.Drawing.Size(15, 24);
+            this.lScore.TabIndex = 3;
+            this.lScore.Text = "l";
+            this.lScore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lScore.Visible = false;
+            // 
+            // ball2
+            // 
+            this.ball2.Image = global::PingPong.Properties.Resources.Ball02;
+            this.ball2.Location = new System.Drawing.Point(515, 72);
+            this.ball2.Name = "ball2";
+            this.ball2.Size = new System.Drawing.Size(20, 20);
+            this.ball2.TabIndex = 2;
+            this.ball2.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.racket.Location = new System.Drawing.Point(314, 394);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(161, 17);
+            this.racket.TabIndex = 1;
+            this.racket.TabStop = false;
+            // 
+            // ball1
+            // 
+            this.ball1.BackColor = System.Drawing.SystemColors.Control;
+            this.ball1.Image = global::PingPong.Properties.Resources.Ball01;
+            this.ball1.Location = new System.Drawing.Point(314, 179);
+            this.ball1.Name = "ball1";
+            this.ball1.Size = new System.Drawing.Size(20, 20);
+            this.ball1.TabIndex = 0;
+            this.ball1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Game";
+            this.Text = "Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.gamePanel.ResumeLayout(false);
+            this.gamePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.PictureBox ball1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ball2;
+        private System.Windows.Forms.Label lScore;
+        private System.Windows.Forms.Label time;
+    }
+}
---
+++ 2_semestr/PingPong/PingPong/Game.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Game : Form
+    {   
+        public double speed1_left = 3, speed1_top = 3;
+        public double speed2_left = 3, speed2_top = 3;
+       
+
+
+        public Game()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.TopMost = true;
+            this.DoubleBuffered = true;
+
+            racket.Top = gamePanel.Bottom - (gamePanel.Bottom / 10);
+        }
+
+        DateTime date1 = new DateTime(0, 0);
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            date1 = date1.AddSeconds(1);
+            time.Text = date1.ToString("mm:ss");
+
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+            ball1.Left += (int)speed1_left;
+            ball1.Top += (int)speed1_top;
+
+
+            if (racket.Bounds.IntersectsWith(ball1.Bounds) && ball1.Bottom >= racket.Top)
+            {
+                if ((ball1.Left < ((racket.Right - racket.Left)/2)))
+                {
+                    speed1_left = -speed1_left;
+                }
+                speed1_left += (racket.Left - ball1.Left) / 100;
+                ball1.Top -= 5;
+                speed1_top += (racket.Left - ball1.Left) / 140;
+
+                speed1_top = -speed1_top;
+                ball1.Refresh();
+            }
+                                 
+            if (ball1.Left <= gamePanel.Left)
+            {
+                speed1_left = -speed1_left;
+            }
+
+            if (ball1.Right >= gamePanel.Right)
+            {
+                speed1_left = -speed1_left;
+            }
+
+            if (ball1.Top <= gamePanel.Top)
+            {
+                speed1_top = -speed1_top;
+            }
+
+            if ((ball1.Bottom >= gamePanel.Bottom) || ((ball1.Left == racket.Right|| ball1.Right == racket.Left) 
+                && ball1.Top >= racket.Top ))
+             
+            {
+                timer1.Enabled = false;
+                Cursor.Show();
+                DialogResult result = MessageBox.Show("Игра окончена!");
+
+
+                if (result == DialogResult.OK)
+                { this.Close(); }
+            }
+
+            ball2.Left -= (int)speed2_left;
+            ball2.Top += (int)speed2_top;
+
+            //ball2.Bottom >= racket.Top && ball2.Left >= racket.Left && ball2.Right <= racket.Right
+            if (racket.Bounds.IntersectsWith(ball2.Bounds) && ball2.Bottom >= racket.Top)
+            {
+                if ((ball2.Left < ((racket.Right - racket.Left) / 2)))
+                {
+                    speed2_left = -speed2_left;
+                }
+                speed2_left += (racket.Left - ball2.Left) / 100;
+                ball2.Top -= 5;
+                speed2_top += (racket.Left - ball2.Left) / 140;
+
+                speed2_top = -speed2_top;
+                ball2.Refresh();
+
+            }
+
+            if (ball2.Left <= gamePanel.Left)
+            {
+                speed2_left = -speed2_left;
+            }
+
+            if (ball2.Right >= gamePanel.Right)
+            {
+                speed2_left = -speed2_left;
+            }
+
+            if (ball2.Top <= gamePanel.Top)
+            {
+                speed2_top = -speed2_top;
+            }
+
+            if ((ball2.Bottom >= gamePanel.Bottom) || ((ball2.Left == racket.Right || ball2.Right == racket.Left)
+                && ball2.Top >= racket.Top ))
+            {
+                timer1.Enabled = false;
+                Cursor.Show();
+                DialogResult result = MessageBox.Show("Игра окончена!");
+
+                if (result == DialogResult.OK)
+                { this.Close(); }
+            }
+
+        }
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close();}
+        }
+    }
+}
